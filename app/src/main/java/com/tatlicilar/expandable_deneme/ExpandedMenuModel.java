ae427020f78579f83f88d0d75b4efa7305032924
rvza <ravzanurkonuk-6034@hotmail.com>
2017-08-18 01:31:54 +0300

    Navigation Drawer added SubItem with Expandable List

---
+++ app/src/main/java/com/tatlicilar/expandable_deneme/ExpandedMenuModel.java
@@ -0,0 +1,24 @@
+package com.tatlicilar.expandable_deneme;
+
+/**
+ * Created by Asus on 17.08.2017.
+ */
+
+public class ExpandedMenuModel {
+
+    String iconName = "";
+    int iconImg = -1; // menu icon resource id
+
+    public String getIconName() {
+        return iconName;
+    }
+    public void setIconName(String iconName) {
+        this.iconName = iconName;
+    }
+    public int getIconImg() {
+        return iconImg;
+    }
+    public void setIconImg(int iconImg) {
+        this.iconImg = iconImg;
+    }
+}
